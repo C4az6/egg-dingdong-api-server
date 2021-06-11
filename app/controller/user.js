715470e1ce86ccc262dbff295d2646556d208142
伤心的瘦子 <54710109+C4az6@users.noreply.github.com>
2021-06-11 13:11:43 +0800

    创建用户表模型和开发注册功能

--- app/controller/user.js
+++ app/controller/user.js
@@ -5,7 +5,27 @@ const Controller = require('egg').Controller;
 class UserController extends Controller {
   // 用户注册
   async reg() {
-    this.ctx.body = '用户注册'
+    let { ctx, app } = this;
+    // 参数验证
+    let { username, password, repassword } = this.ctx.request.body
+    // 验证用户是否已经存在
+    let data = await app.model.User.findOne({
+      where: {
+        username
+      }
+    })
+    if (data) {
+      ctx.throw(400, '用户名已存在')
+    }
+    // 创建用户
+    let user = await app.model.User.create({
+      username,
+      password
+    });
+    if(!user) {
+      ctx.throw(400, '创建用户失败');
+    }
+    ctx.apiSuccess(user);
   }
 }
 
